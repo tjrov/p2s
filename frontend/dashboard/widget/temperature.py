dbae62bf1f024852c20a2208940add1bfc4c2d45
achenvc <45048476+achenvc@users.noreply.github.com>
2019-12-04 14:43:27 -0500

    started

--- frontend/dashboard/widget/temperature.py
+++ frontend/dashboard/widget/temperature.py
@@ -1,10 +1,11 @@
 from .widget import Widget
-
+from tkinter import Tk, Canvas
+from tkinter.font import Font
 
 class TemperatureWidget(Widget):
-
     WIDTH = 250
     HEIGHT = 150
+    THRESHOLD = 100
 
     def __init__(self, left_corner, canvas):
         self.left_x, self.left_y = left_corner
@@ -17,6 +18,19 @@ class TemperatureWidget(Widget):
             fill="black",
             outline="yellow",
         )
+        fill = "red"
+        font = Font(family="Times", size=20, weight="bold")
+        (w, h) = (font.measure("100"), font.metrics("linespace"))
+
+        self.textLabel = self.canvas.create_text(self.left_x + self.WIDTH/2 - w/2, self.left_y + self.HEIGHT/2 - h/2, fill=fill, font="Times 20 italic bold",
+                                text="100")
 
     def update(self, data: dict):
-        raise NotImplementedError
+        temperature = data["temperature"]
+        if temperature > self.THRESHOLD:
+            fill = "red"
+        else:
+            fill = "blue"
+
+        # self.textLabel = dict["temperature"]
+
