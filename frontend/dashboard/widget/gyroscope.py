60bb187eb41daf08c0fdf69282495754adef16ac
Corporal98 <45048532+Corporal98@users.noreply.github.com>
2019-12-07 17:48:38 -0500

    commit and write gyroscope

--- frontend/dashboard/widget/gyroscope.py
+++ frontend/dashboard/widget/gyroscope.py
@@ -9,14 +9,8 @@ class GyroscopeWidget(Widget):
     def __init__(self, left_corner, canvas):
         self.left_x, self.left_y = left_corner
         self.canvas = canvas
-        canvas.create_rectangle(
-            self.left_x,
-            self.left_y,
-            self.left_x + self.WIDTH,
-            self.left_y + self.HEIGHT,
-            fill="magenta",
-            outline="yellow",
-        )
+        self.text_id = canvas.create_text(125, 75, font=("Serif", "25"))
+        self.update({"gyro" : 2048})
 
     def update(self, data: dict):
-        raise NotImplementedError
+        self.canvas.itemconfig(self.text_id, text=str(data["gyro"])+"°/s")
