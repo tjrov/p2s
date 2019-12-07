165f765f4d195b8a9ae309a05ba667c209e28427
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-12-07 17:45:50 -0500

    clean up

--- frontend/dashboard/widget/temperature.py
+++ frontend/dashboard/widget/temperature.py
@@ -5,16 +5,20 @@ class TemperatureWidget(Widget):
     WIDTH = 250
     HEIGHT = 150
     THRESHOLD = 32
-    DEGREE_SIGN = u'\N{DEGREE SIGN}'
+    DEGREE_SIGN = "\N{DEGREE SIGN}"
 
     def __init__(self, left_corner, canvas):
         self.left_x, self.left_y = left_corner
         self.canvas = canvas
 
         fill = "red"
-        self.text_label = self.canvas.create_text(self.left_x + self.WIDTH / 2, self.left_y + self.HEIGHT / 2,
-                                                  fill=fill, font="Serif 25",
-                                                  text="old")
+        self.text_label = self.canvas.create_text(
+            self.left_x + self.WIDTH / 2,
+            self.left_y + self.HEIGHT / 2,
+            fill=fill,
+            font="Serif 25",
+            text=f"0{self.DEGREE_SIGN}F",
+        )
 
     def update(self, data: dict):
         temperature = data["temperature"]
@@ -23,4 +27,6 @@ class TemperatureWidget(Widget):
         if temperature < self.THRESHOLD:
             fill = "blue"
 
-        self.canvas.itemconfig(self.text_label, fill=fill, text=f"{temperature}{self.DEGREE_SIGN}F")
+        self.canvas.itemconfig(
+            self.text_label, fill=fill, text=f"{temperature}{self.DEGREE_SIGN}F"
+        )
