11693ee6456d0fafc253bc87e83000892289d1cd
achenvc <45048476+achenvc@users.noreply.github.com>
2019-12-07 17:41:04 -0500

    edit

--- frontend/dashboard/widget/temperature.py
+++ frontend/dashboard/widget/temperature.py
@@ -1,31 +1,26 @@
 from .widget import Widget
-from tkinter import Tk, Canvas
-from tkinter.font import Font
+
 
 class TemperatureWidget(Widget):
     WIDTH = 250
     HEIGHT = 150
-    THRESHOLD = 100
+    THRESHOLD = 32
+    DEGREE_SIGN = u'\N{DEGREE SIGN}'
 
     def __init__(self, left_corner, canvas):
         self.left_x, self.left_y = left_corner
         self.canvas = canvas
 
         fill = "red"
-        font = Font(family="SansSerif", size=50, weight="bold")
-        degree_sign = u'\N{DEGREE SIGN}'
-        (w, h) = (font.measure("100 " + degree_sign + "F"), font.metrics("linespace"))
-
-        self.textLabel = self.canvas.create_text(self.left_x + self.WIDTH/2, self.left_y + self.HEIGHT/2, fill=fill, font="SansSerif 50 bold",
-                                text="100 " + degree_sign + "F")
-
+        self.text_label = self.canvas.create_text(self.left_x + self.WIDTH / 2, self.left_y + self.HEIGHT / 2,
+                                                  fill=fill, font="Serif 25",
+                                                  text="old")
 
     def update(self, data: dict):
         temperature = data["temperature"]
-        if temperature > self.THRESHOLD:
-            fill = "red"
-        else:
+
+        fill = "red"
+        if temperature < self.THRESHOLD:
             fill = "blue"
 
-        # self.textLabel = dict["temperature"]
-
+        self.canvas.itemconfig(self.text_label, fill=fill, text=f"{temperature}{self.DEGREE_SIGN}F")
