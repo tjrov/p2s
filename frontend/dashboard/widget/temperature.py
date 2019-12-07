e113327a89f6f0a4d464058412ee5c098f1362f2
achenvc <45048476+achenvc@users.noreply.github.com>
2019-12-07 15:19:55 -0500

    fixed centering and added degree symbol

--- frontend/dashboard/widget/temperature.py
+++ frontend/dashboard/widget/temperature.py
@@ -19,11 +19,13 @@ class TemperatureWidget(Widget):
             outline="yellow",
         )
         fill = "red"
-        font = Font(family="Times", size=20, weight="bold")
-        (w, h) = (font.measure("100"), font.metrics("linespace"))
+        font = Font(family="SansSerif", size=50, weight="bold")
+        degreeSign = u'\N{DEGREE SIGN}'
+        (w, h) = (font.measure("100 " + degreeSign + "F"), font.metrics("linespace"))
 
-        self.textLabel = self.canvas.create_text(self.left_x + self.WIDTH/2 - w/2, self.left_y + self.HEIGHT/2 - h/2, fill=fill, font="Times 20 italic bold",
-                                text="100")
+        self.textLabel = self.canvas.create_text(self.left_x + self.WIDTH/2, self.left_y + self.HEIGHT/2, fill=fill, font="SansSerif 50 bold",
+                                text="100 " + degreeSign + "F")
+
 
     def update(self, data: dict):
         temperature = data["temperature"]
